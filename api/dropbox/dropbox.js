47c461501a10ebb2c4fe9b68371e29cd8c2ad346
nyinyi979 <nyinyi095062687@gmail.com>
2023-11-08 22:44:32 +0630

    dropbox_fixed

--- api/dropbox/dropbox.js
+++ api/dropbox/dropbox.js
@@ -3,37 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dropbox = void 0;
 var path = require("path");
 var dotenv = require("dotenv");
-var axios = require('axios');
-const express = require('express');
-const router = express.Router();
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-let access_token = 'sl.BpcjqqsV27ffpOWfW6GcDpF_cbpB9M1Kc8yE6tOT-6jz1Ooqesg2LhIcl8c_0yndJ-CIvHYBIvWORkwfYit1eAyfuWq902bLYI3eflcs-QZTlOESCu9FIghlJ5EJKaS1jMcdK1V75JOEbBbeHutFkaE';
-setInterval(()=>{
-    axios.post(
-        'https://api.dropboxapi.com/oauth2/token',
-        new URLSearchParams({
-        'code': process.env.NEXT_PUBLIC_DROPBOX_KEY,
-        'grant_type': 'authorization_code',
-        'client_id' : process.env.NEXT_PUBLIC_DROPBOX_ID,
-        'client_secret': process.env.NEXT_PUBLIC_DROPBOX_SECRET,
-        })
-    ).then((res)=>{
-        console.log(res);
-        access_token = res.data.access_token;
-    }).catch((err)=>{
-        console.log(err)
-    })
 
-}, 14400)
-console.log(access_token)
-// add custom path here 
-// server.post('/request/custom', custom);  
-router.get('/getToken', async(req, res)=>{
-    res.send(access_token);
-})
 var Dropbox = require("dropbox");
 //GLOBAL DROPBOX OBJECT - ONLY FOR SERVER ( DOESN'T WANT TO INITIALIZE EVERYTIME )
-exports.dropbox = new Dropbox.Dropbox({ accessToken: process.env.NEXT_PUBLIC_DROPBOX_TOKEN });
+exports.dropbox = new Dropbox.Dropbox({accessToken: process.env.NEXT_PUBLIC_REFRESH_TOKEN , clientId: process.env.NEXT_PUBLIC_DROPBOX_ID , clientSecret: process.env.NEXT_PUBLIC_DROPBOX_SECRET});
 //npx tsc ./api/dropbox/dropbox.ts
-
-exports.token = router
--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const cors = require("cors");
 const { blogRouter } = require("./api/routes/blogs");
 const { imageRouter } = require("./api/routes/createImage");
 const { userRouter } = require("./api/routes/user");
-const axios = require('axios');
 const path = require('path')
 var dotenv = require("dotenv");
-const { token } = require("./api/dropbox/dropbox");
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 app.use('/api',imageRouter);
 app.use(blogRouter);
 app.use(userRouter);
-app.use(token)
 app.use(cors({origin: ['https://blogs-app-one.vercel.app','http://blogs-app-one.vercel.app', 'http://localhost:3000']}))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
