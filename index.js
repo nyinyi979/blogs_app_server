c84df4046c7e7d9a4f4f5f55d418fe8e58f08829
nyinyi979 <nyinyi095062687@gmail.com>
2023-11-08 17:43:44 +0630

    dropbox oauth

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const cors = require("cors");
 const { blogRouter } = require("./api/routes/blogs");
 const { imageRouter } = require("./api/routes/createImage");
 const { userRouter } = require("./api/routes/user");
-const { readUserbyEmail, deleteUser, readUserbyID, createUser } = require("./api/prisma_methods/users/CRUD");
-const { default: prisma } = require("./lib/prisma_object");
-const { blogsByCategory } = require("./api/prisma_methods/blogs/searchBlogs");
-
+const axios = require('axios');
+const path = require('path')
+var dotenv = require("dotenv");
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 app.use('/api',imageRouter);
 app.use(blogRouter);
 app.use(userRouter)
@@ -17,8 +17,24 @@ app.use(cors({origin: ['https://blogs-app-one.vercel.app','http://blogs-app-one.
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+let access_token = 'sl.BpfVtQy4JTeHTVrxoGdcpTSNqMkiDGwDEdcFG4xNYbBoRw0iLgFRneBGEUYPXc6oDZWgSvYZrZuL8_96p2tucP7TCczuUppP2cTG3PU_HDiiW_upKebvoohShAqgXXMYLRctMnCu-9xpR_KNicBmRHs';
+setInterval(()=>{
+    axios.post(
+        'https://api.dropboxapi.com/oauth2/token',
+        new URLSearchParams({
+        'code': process.env.NEXT_PUBLIC_DROPBOX_KEY,
+        'grant_type': 'authorization_code',
+        'client_id' : process.env.NEXT_PUBLIC_DROPBOX_ID,
+        'client_secret': process.env.NEXT_PUBLIC_DROPBOX_SECRET,
+        })
+    ).then((res)=>{
+        console.log(res);
+        access_token = res.data.access_token;
+    }).catch((err)=>{
+        console.log(err)
+    })
 
-
+}, 14400)
 // add custom path here 
 // server.post('/request/custom', custom);  
 
@@ -27,7 +43,6 @@ app.get('/',async(req , res)=>{
 })
 
 app.listen(5000, async() => {
-    
 	console.log('Ready on http://localhost:5000');
 })
 //npx nodemon server/index 
